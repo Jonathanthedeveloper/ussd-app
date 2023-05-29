3efcb8d5559eca6796708fb2ba1822e47f334f16
jonathan <82114500+Jonathanthedeveloper@users.noreply.github.com>
2023-05-29 09:24:29 +0100

    FIX: savedcodes avalability in inner menu
    CHORE : improved pwa caching strategy

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,16 +4,16 @@ import MenuButton from "./MenuButton.jsx";
 
 function Menu({activeTab}) {
     return (
-        <section className={`absolute w-full ${activeTab === 1? "block": "hidden"} p-5 transition`}>
-            <div className="grid gap-5  text-gray-700">
-                <div className="grid gap-5">
-                    <MenuButton label="MTN Codes" to="mtn"/>
-                    <MenuButton label="Airtel Codes" to="airtel"/>
-                    <MenuButton label="9Mobile" to="9mobile"/>
-                    <MenuButton label="Glo" to="glo"/>
+            <section className={`absolute w-full ${activeTab === 1 ? "block" : "hidden"} p-5 transition`}>
+                <div className="grid gap-5  text-gray-700">
+                    <div className="grid gap-5">
+                        <MenuButton label="MTN Codes" to="mtn"/>
+                        <MenuButton label="Airtel Codes" to="airtel"/>
+                        <MenuButton label="9Mobile" to="9mobile"/>
+                        <MenuButton label="Glo" to="glo"/>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
     )
 }
 
